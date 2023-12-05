16701e6fea5281de5e3bd0199c3bd8c529b450e4
Adedax39 <lasathrathnayake@gmail.com>
2023-12-05 08:06:48 +0530

    Skills

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,12 @@
-
 import HeroSection from "../HeroSection.jsx";
 import Navbar from "../Navbar.jsx";
+import MySkills from "../Myskills.jsx";
 
 function Home(){
   return(  <>
     <Navbar />
     <HeroSection />
+    <MySkills />
     </>)
 };
 
